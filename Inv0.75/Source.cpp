30a08eac2edb3bfe2a9e10e9f3ee91d35a4339ae
Zahon7 <81902474+Zahon7@users.noreply.github.com>
2022-04-11 18:46:52 +0200

    Added version 0.75 (Bug Fix, ...)
    Fixed: Crash when you try to use a undefined variable, also "some" variable issues
    Added: Number of line in error

---
+++ Inv0.75/Source.cpp
@@ -0,0 +1,237 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+const std::string WHITESPACE = " \n\r\t\f\v";
+
+std::string ltrim(const std::string &s)
+{
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+
+// for string delimiter
+vector<string> split (string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+        token = s.substr (pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back (token);
+    }
+
+    res.push_back (s.substr (pos_start));
+    return res;
+}
+
+#include <sys/stat.h>
+int Exists(const char *name)
+{
+  struct stat   buffer;
+  return (stat (name, &buffer) == 0);
+}
+
+int count(string s) {
+  int count = 0;
+
+  for (int i = 0; i < s.size(); i++)
+    if (s[i] == '.') count++;
+
+  return count;
+}
+
+bool findInList(char* list[], string what, int lenght) {
+    for(int i = 0; i < lenght; i++) {
+        if(list[i] == what) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::string variables_keys[100] = {};
+std::string variables_vals[100] = {};
+int pointer = 0;
+
+void insert(std::string key, std::string val) {
+    variables_keys[pointer] = key;
+    variables_vals[pointer] = val;
+    pointer += 1;
+}
+
+bool isInList(std::string what) {
+    for(int i = 0; i < 101; i++) {
+        if(what == variables_keys[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int getindex(std::string what) {
+    for(int i = 0; i < 101; i++) {
+        if(variables_keys[i] == what)
+            return i;
+    }
+    return -1;
+}
+
+std::string getValue(std::string what) {
+    std::string out = split(variables_vals[getindex(what)], ": ")[1];
+    if(out[0] == '\"') {
+        return out.substr(0, out.length() - 1).substr(1, out.length());
+    }
+    return out;
+}
+
+std::string getVars() {
+    std::string Out = "";
+    for(int i = 0; i < pointer + 1; i++) {
+        Out += variables_keys[i] + " = " + variables_vals[i] + "\n";
+    }
+    Out = Out.substr(0, Out.length() - 5);
+    return Out;
+}
+
+/*
+some
+text: Hello
+num: 10
+bit: 0
+*/
+
+bool is_number(std::string s)
+{
+    for(int i = 0; i < s.length(); i++) {
+        if(isdigit(s[i])) {
+            //
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+int main(int argc, char* argv[]) {
+    if(argc == 1) {
+        std::cout << "Using: Innovation.exe [fileName] [options]";
+        exit(0);
+    }
+    // Create a text string, which is used to output the text file
+    string myText;
+
+    // Setup buildin variables
+    insert("HW", "text: \"Hello, world!\"");
+    insert("YES", "bit: 1");
+    insert("NO", "bit: 0");
+    insert("SOME", "some");
+
+    int line = 0;
+
+    
+
+    // Read from the text file
+    ifstream MyReadFile(argv[1]);
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline (MyReadFile, myText)) {
+            myText = ltrim(myText);
+        if(myText.back() == '.') {
+            insert(myText.substr(0, myText.length() - 1), "some: some");
+        }
+        else if(myText.rfind("text: ", 0) == 0) {
+            std::string varName = split(split(myText, ": ")[1], " = ")[0];
+            if(split(split(myText, ": ")[1], " = ")[1].rfind("\"", 0) == 0 && myText.back() == '"') {
+                insert(varName, "text: " + split(split(myText, ": ")[1], " = ")[1]);
+            } else {
+                std::cout << "ERROR: Line " << line << ": The text variable must start and end with \"" << std::endl;
+            }
+        }
+        else if(myText.rfind("num: ", 0) == 0) {
+            std::string varName = split(split(myText, ": ")[1], " = ")[0];
+            if(is_number(split(split(myText, ": ")[1], " = ")[1])) {
+                insert(varName, "num: " + split(split(myText, ": ")[1], " = ")[1]);
+            } else {
+                std::cout << "ERROR: Line " << line << ": The number variable must be number :D" << std::endl;
+            }
+            
+        }
+        else if(myText.find(" = ") != std::string::npos && split(myText, " ")[1] == "=") {
+            std::string value = split(myText, " = ")[1];
+            std::string type = "?";
+            if(value[0] == '\"') {
+                type = "text";
+                insert(split(myText, " = ")[0], type + ": " + value);
+            } else if(is_number(value)) {
+                type = "num";
+                insert(split(myText, " = ")[0], type + ": " + value);
+            } else if(value == "some") {
+                type = "some";
+                insert(split(myText, " = ")[0], type + ": " + value);
+            } else if(value == "yes") {
+                type = "bit";
+                value = "1";
+                insert(split(myText, " = ")[0], type + ": " + value);
+            } else if(value == "no") {
+                type = "bit";
+                value = "0";
+                insert(split(myText, " = ")[0], type + ": " + value);
+            } else {
+                std::cout << "ERROR: Line " << line << ": Invalid variable type \"" << myText << "\"" << std::endl;
+            }
+            
+        }
+        else if(myText.rfind("bit: ", 0) == 0) {
+            std::string varName = split(split(myText, ": ")[1], " = ")[0];
+            if (split(split(myText, ": ")[1], " = ")[1] == "0" || split(split(myText, ": ")[1], " = ")[1] == "1") {
+                insert(varName, "bit: " + split(split(myText, ": ")[1], " = ")[1]);
+            } else {
+                std::cout << "ERROR: Line " << line << ": The bit variable must be 1 or 0" << std::endl;
+            }
+        }
+        else if(myText.rfind("some: ", 0) == 0) {
+            std::string varName = split(myText, ": ")[1];
+            insert(varName, "some: some");
+        }
+        else if(myText.rfind("write(", 0) == 0) {
+            std::string out = split(myText, "(")[1].substr(0, split(myText, "(")[1].length() - 1);
+            if(isInList(out)) {
+                std::cout << getValue(out) << std::endl;
+            } else {
+                std::cout << "ERROR: Line " << line << ": the variable \"" << out << "\" isn't defined" << std::endl;
+            }
+        }
+        else if(myText.rfind("put(", 0) == 0) {
+            std::string out = split(myText, "(")[1].substr(0, split(myText, "(")[1].length() - 1);
+            std::cout << getValue(out);
+        }
+        else if(myText == "vars()") {
+            std::cout << getVars() << std::endl;
+        }
+        else if(myText == "" || myText.rfind("/", 0) == 0) {
+            // Good
+        } else {
+            std::cout << "ERROR: Line " << line << ": Invalid syntax \"" << myText << "\"" << std::endl;
+            exit(0);
+        }
+        //if(inString(myText, "=")) {
+        //    if(split(myText, "="))
+        //}
+        //if (myText.rfind(".-", 0) == 0) {
+        //    std::cout << count(split(myText, "-")[1]) << std::endl;
+        //}
+        line += 1;
+    }
+    // Close the file
+    MyReadFile.close();
+
+    return 0;
+}
